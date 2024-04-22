a0296ee34b1f162bca8931fa5e490cadc953608f
Artem Melnyk <melkam464@gmail.com>
2024-04-22 14:50:02 +0200

    add placeholder main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
 	site: "https://melkam.dev",
+	integrations: [tailwind()],
 });
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{js,ts,jsx,tsx,vue,astro,mdx}"],
+	theme: {
+		fontFamily: {
+			sans: [
+				"Switzer",
+				"-apple-system",
+				"BlinkMacSystemFont",
+				"Roboto",
+				"Helvetica",
+				"sans-serif",
+			],
+		},
+	},
+};
